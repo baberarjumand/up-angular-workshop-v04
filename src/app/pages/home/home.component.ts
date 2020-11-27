0bcf7beaa7e9065c8d34ef14d787c730822e285d
baberarjumand <baber.arjumand@gmail.com>
2020-11-27 17:07:51 +0500

    displayed company data as cards in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Company } from './../../model/company.model';
 import { CompaniesDbService } from './../../services/companies-db.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  companiesArr: Company[] = [];
+  isLoading = true;
+
   constructor(private companyDbService: CompaniesDbService) {}
 
   ngOnInit(): void {
-    this.companyDbService.getAllCompanies().subscribe((companiesArr) => {
-      console.log(companiesArr);
+    this.companyDbService.getAllCompanies().subscribe((tempCompaniesArr) => {
+      console.log(tempCompaniesArr);
+      this.companiesArr = tempCompaniesArr;
+      this.isLoading = false;
+
+      // setTimeout(() => {
+      //   this.isLoading = false;
+      // }, 5000);
     });
   }
 }
